bdb4f66d09d13bbae2de4371bd3856602ba51f1c
luisvalle7 <luisluisxv64@gmail.com>
2025-12-03 04:24:49 -0400

    Add chapter 1 content: project setup and home page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>¡Hola Mundo desde Next.js!</h1>
+      <p>Este es mi primer proyecto en Next.js.</p>
+    </main>
+  );
+}
